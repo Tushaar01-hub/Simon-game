2ed9219dbffb234b9c19542ea01d9488a5000114
Tushaar01-hub <tushaarchamp@gmail.com>
2025-07-12 10:30:38 +0530

    Update game.js

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -6,20 +6,20 @@ var started = false;
 
 var level = 0;
 
-// $(document).keypress(function () {
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+// $(document).on("keypress click", function () {
 //   if (!started) {
 //     //$("#level-title").text("Level " + level);
 //     nextSequence();
 //     started = true;
 //   }
 // });
-$(document).on("keypress click", function () {
-  if (!started) {
-    //$("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
-  }
-});
 
 $(".btn").click(function(){
     var UserChosenColor = $(this).attr("id");
@@ -33,8 +33,8 @@ $(".btn").click(function(){
 
 
 function checkAnswer(currentLevel) {
-  console.log("User clicked pattern length:", userClickedPattern.length);
-  console.log("Expected full pattern length:", gamePattern.length);
+  // console.log("User clicked pattern length:", userClickedPattern.length);
+  // console.log("Expected full pattern length:", gamePattern.length);
   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
     console.log("Success");
     if (userClickedPattern.length === gamePattern.length) {
@@ -47,10 +47,11 @@ function checkAnswer(currentLevel) {
     playSound("wrong");
     
     $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
     setTimeout(function () {
       $("body").removeClass("game-over");
     }, 200);
-    $("#level-title").text("Game Over, Press Any Key to Restart");
+    
     startOver();
   }
 }
@@ -65,8 +66,8 @@ function nextSequence() {
   var randomChosenColor = buttoncolours[randomnumber];
   gamePattern.push(randomChosenColor);
 
-  console.log("Next color:", randomChosenColor);
-  console.log("Full pattern:", gamePattern);
+  // console.log("Next color:", randomChosenColor);
+  // console.log("Full pattern:", gamePattern);
 
   $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
